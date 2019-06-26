79ab325a6be0fa2b17126962e22fadf8b181d840
aale flor <floresb.alejandra@gmail.com>
2019-06-26 13:00:18 -0700

    feat: view of monitoring

---
+++ src/monitor/Monitoreo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace monitor
+{
+    /// <summary>
+    /// Interaction logic for Monitoreo.xaml
+    /// </summary>
+    public partial class Monitoreo : Window
+    {
+        public Monitoreo()
+        {
+            InitializeComponent();
+        }
+    }
+}
